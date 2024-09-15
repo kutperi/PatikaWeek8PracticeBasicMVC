e993e19947fa318ad5fb14412d32ef56a879becd
Bilal <bilalbilin12@gmail.com>
2024-09-15 04:14:37 +0300

    Add project files.

---
+++ PatikaWeek8PracticeBasicMVC/Controllers/CustomerOrdersController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PatikaWeek8PracticeBasicMVC.Models;
+
+namespace PatikaWeek8PracticeBasicMVC.Controllers
+{
+    public class CustomerOrdersController : Controller
+    {
+        public IActionResult Index()
+        {
+            var customerOrderViewModel = new CustomerOrderViewModel(); // View Model oluşturuluyor.
+
+            var customer = new Customer()
+            {
+                Id = 1,
+                FirstName = "Bilal",
+                LastName = "Bilin",
+                Email = "bb@gmail.com"
+            };
+            
+            customerOrderViewModel.customer = customer;
+
+            var orders = new List<Order>
+            {
+                new Order { Id = 1, ProductName = "Elma", Price = 26.00m, Quantity = 1000},
+                new Order { Id = 2, ProductName = "Armut", Price = 20.00m, Quantity = 5000},
+                new Order { Id = 3, ProductName = "Şeftali", Price = 30.00m, Quantity = 2500},
+                new Order { Id = 4, ProductName = "Çilek", Price = 10.00m, Quantity = 12000},
+                new Order { Id = 5, ProductName = "Karpuz", Price = 40.00m, Quantity = 2000},
+                new Order { Id = 6, ProductName = "Kavun", Price = 50.00m, Quantity = 1500},
+
+            };
+
+            customerOrderViewModel.orders = orders;
+
+            return View(customerOrderViewModel); // View Model gönderiliyor.
+        }
+    }
+}
---
+++ PatikaWeek8PracticeBasicMVC/Models/CustomerOrderViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace PatikaWeek8PracticeBasicMVC.Models
+{
+    public class CustomerOrderViewModel
+    {
+        public Customer customer {  get; set; }
+        public List<Order> orders { get; set; }
+    }
+}
